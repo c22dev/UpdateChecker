1bc0febc23dfc58f5c61ae45f24e5ca33316ab46
c22dev <constclerc@gmail.com>
2022-12-13 08:02:27 +0100

    Initial Commit

---
+++ UpdateChecker/UpdateCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpdateCheckerApp.swift
+//  UpdateChecker
+//
+//  Created by Constantin Clerc on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct UpdateCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
